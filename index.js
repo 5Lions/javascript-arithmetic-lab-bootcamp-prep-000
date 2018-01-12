3c23fb3d62d739fabd158f21bcc6fe310e2497fc
A Lee <ayslee@gmail.com>
2018-01-12 18:54:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 function add(a, b) {
-  return(parseInt(a) + parseInt(b));
+  return(a + b);
 }
 
 console.log("add " + add(1, 2));
 
 function subtract(a, b) {
-  return(parseInt(a) - parseInt(b));
+  return(a - b);
 }
 
 console.log("subtract " + subtract(4, 3));
 
 function multiply(a, b) {
-  return(parseInt(a) * parseInt(b));
+  return(a * b);
 }
 
 console.log("multiply " + multiply(2, 3));
 
 function divide(a, b) {
-  return(parseInt(a)/parseInt(b));
+  return(a/b);
 }
 
 console.log("divide " + divide(20,5));
 
 function inc(a) {
-  return(parseInt(a)+1);
+  return(a+1);
 }
 
 console.log("increment " + inc(23));
